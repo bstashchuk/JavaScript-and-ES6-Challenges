869b0c9baaed9ef85fdaa0de2b9fea87de2ff1e4
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-09-17 23:23:01 +0300

    Final

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-/* CHALLENGE 16 - Swap values of the two variables
+/* CHALLENGE 17 - IIFE (Immediately Invoked Function Expression)
 
-Swap values of the a and b.
-Don't use for this any new variable.
+Complete IIFE that will expose following methods:
+  greet() - It will take one argument and return greeting string
+  changeGreeting() - It will change greeting string
+
+Initial greeting string "Hey, that's" must be defined inside IIFE.
 */
 
 "use strict";
 
-var a = "first";
-var b = "second";
+// var greeting = (Write IIFE here);
 
-[b, a] = [a, b];
+console.log(greeting.greet("Bob"));
+// Hey, that's Bob
 
-console.log(a, b);
-// second first
+console.log(
+  greeting.changeGreeting("Good Morning from")
+);
+// undefined
+
+console.log(greeting.greet("Emily"));
+// Good Morning from Emily
