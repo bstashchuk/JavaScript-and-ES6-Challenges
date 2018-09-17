57614348d97eade67e0f7f1e79066669c5f8cc8c
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-09-17 23:19:02 +0300

    Final

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-/* CHALLENGE 12 - Copy Array
+/* CHALLENGE 13 - Template Literals
 
-Create copy of the a array.
+Create carInfo() function that
+will return info about each car.
+
+Car is considered cheap if it's price is <= 20000
+Car is considered expensive it it's price is > 20000
 */
 
 "use strict";
 
-var a = [1, 2, 3];
+var cars = [
+  { brand: "Honda", price: 13000 },
+  { brand: "Rolls-Royce", price: 120000 }
+];
 
-var b;
+// Write carInfo() function here
 
-// b = a.slice();
-b = [...a];
+cars.forEach(car => console.log(carInfo(car)));
+/* Price of my new Honda is 13000$
+ and it is cheap car. */
 
-b.push("newElement");
-
-console.log(a);
-// [1, 2, 3]
-
-console.log(b);
-// [1, 2, 3, "newElement"]
+/* Price of my new Rolls-Royce
+ is 120000$ and it is expensive car. */
