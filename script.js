c5fb34d5016b85c923eac78184e155902e07c658
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-09-17 23:10:31 +0300

    Final

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-/* CHALLENGE 4 - Hoisting
+/* CHALLENGE 5 - Ternary Operator
 
-Change code to fix first error after the line 15.
-Error after the line 20 should still be generated.
+Change contents of the isNumber function
+using ternary operator.
 */
 
 "use strict";
 
-var a = 5,
-  b = 10;
-
-if (b > a) {
-  let c = 2;
-  c = a + b + c;
-  console.log(c);
-  // BEFORE: Uncaught ReferenceError: c is not defined
-  // AFTER: 17
+function isNumber(a) {
+  if (typeof a === "number") {
+    return "That's number";
+  } else {
+    return "That's not a number";
+  }
 }
 
-console.log(c);
-// Uncaught ReferenceError: c is not defined
+console.log(isNumber(10));
+// That's number
+
+console.log(isNumber("Hey there"));
+// That's not a number
+
+console.log(isNumber(true));
+// That's not a number
