339e1eacd86163519944fe58057cc2131ab306b7
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-09-17 23:26:35 +0300

    Final

--- script.js
+++ script.js
@@ -14,7 +14,14 @@ It should return an object like this:
 
 var nums = [10, -12, 30, -1, -8, 0, 14, -33, 20];
 
-// Write sumPlusMinus() function here
+function sumPlusMinus(arr) {
+  let obj = {};
+  nums.forEach(el => {
+    if (el > 0) obj.plus = (obj.plus || 0) + el;
+    else if (el < 0) obj.minus = (obj.minus || 0) + el;
+  });
+  return obj;
+}
 
 console.log(sumPlusMinus(nums));
 // {plus: 74, minus: -54}
