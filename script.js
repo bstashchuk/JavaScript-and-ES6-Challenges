4f02ab1e4097f8745600e44378eab94fffd8fa14
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-09-17 23:04:07 +0300

    Final

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+Create a function sum() that will sum all arguments passed to it.
+Quantity of the arguments is unknown.
+
+Use console.log() at the end of the sum() function to print result.
+Use ES6 whenever possible.
+*/
+
+"use strict";
+
+// Write code here
+
+sum(1, 3);
+//4
+
+sum(10, 20, 5);
+//35
+
+sum(2, 5, 80, 1, 10, 12);
+//110
