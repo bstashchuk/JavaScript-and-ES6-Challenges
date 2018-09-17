1cfce647092625c5b68644096a4959049e18fe85
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-09-17 23:21:12 +0300

    Final

--- script.js
+++ script.js
@@ -11,7 +11,13 @@ var vowels = ["a", "e", "i", "o", "u"];
 
 var str = "Today is best day of my life";
 
-// Write code here
+// for (let i = 0; i < str.length; i++) {
+//   if (vowels.includes(str[i])) vowelsCount++;
+// }
+
+for (let s of str) {
+  if (vowels.includes(s)) vowelsCount++; // ES2016 (ES7) syntax
+}
 
 console.log(vowelsCount);
 // 8
