1e25e47980b81e6c1b458be69de1be929b3b07f5
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-09-17 23:11:49 +0300

    Final

--- script.js
+++ script.js
@@ -6,11 +6,7 @@ where possible.
 
 "use strict";
 
-function mult(a, b) {
-  return a * b;
-}
+var mult = (a, b) => a * b;
 
-setTimeout(function() {
-  console.log(mult(5, 10));
-}, 1000);
+setTimeout(() => console.log(mult(5, 10)), 1000);
 // 50
