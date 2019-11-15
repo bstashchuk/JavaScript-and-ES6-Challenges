fcaed687b1b93454e5611c20ef24aa4fa04ec3e2
billalp <billal.patel@rightmove.co.uk>
2019-11-15 11:28:39 +0000

    Completed challenge 5 and rename challenge 1

---
+++ challenge-5/script.js
@@ -0,0 +1,19 @@
+/* CHALLENGE 5 - Ternary Operator
+Change contents of the isNumber function
+using ternary operator.
+*/
+
+'use strict';
+
+function isNumber(a) {
+  return (typeof(a) === 'number' ? 'Thats number' : 'Thats not a number');
+}
+
+console.log(isNumber(10));
+// That's number
+
+console.log(isNumber("Hey there"));
+// That's not a number
+
+console.log(isNumber(true));
+// That's not a number
