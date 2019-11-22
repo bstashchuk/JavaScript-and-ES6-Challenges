f764dd0c1381b86bca6ac55be2af958ab3dd9c5e
billalp <billal.patel@rightmove.co.uk>
2019-11-22 19:08:36 +0000

    Completed

---
+++ challenge-10/script.js
@@ -0,0 +1,21 @@
+/* CHALLENGE 10 - Destructuring and Rest Operator
+Assign values to the a, b, c variables
+using destructuring and rest operator.
+*/
+
+'use strict';
+
+// var a, b, c;
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+const [ a, b, ...c ] = arr;
+
+console.log(a);
+// 1
+
+console.log(b);
+// 2
+
+console.log(c);
+// [3, 4, 5, 6, 7]
