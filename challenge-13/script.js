ed901d0adca0c9114e688ff55418735fa61aa2bd
billalp <billal.patel@rightmove.co.uk>
2019-12-07 22:49:05 +0000

    Completed

--- challenge-13/script.js
+++ challenge-13/script.js
@@ -5,18 +5,22 @@ Car is considered cheap if it's price is <= 20000
 Car is considered expensive it it's price is > 20000
 */
 
-"use strict";
+'use strict';
 
-var cars = [
-  { brand: "Honda", price: 13000 },
-  { brand: "Rolls-Royce", price: 120000 }
+const cars = [
+    { 
+        brand: 'Honda', 
+        price: 13000 
+    },
+    { 
+        brand: 'Rolls-Royce', 
+        price: 120000 
+    }
 ];
 
-// Write carInfo() function here
+const carInfo = (car) => {
+    let value = car.price <= 20000 ? "cheap" : "expensive";
+    return `${car.brand} = ${value}`;
+}
 
 cars.forEach(car => console.log(carInfo(car)));
-/* Price of my new Honda is 13000$
- and it is cheap car. */
-
-/* Price of my new Rolls-Royce
- is 120000$ and it is expensive car. */
